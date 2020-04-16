5e518842b4bf979832384505ba54b60826d6fd27
Padang Basudewa <padangbasudewa4@gmail.com>
2020-04-16 15:02:55 +0700

    firebase + styles.css

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCJkZBtdL6-fXs7wx4hK6SwfkrO9AkU_PI",
+    authDomain: "modul10-b87d0.firebaseapp.com",
+    databaseURL: "https://modul10-b87d0.firebaseio.com",
+    projectId: "modul10-b87d0",
+    storageBucket: "modul10-b87d0.appspot.com",
+    messagingSenderId: "731046224277",
+    appId: "1:731046224277:web:d613e5dbf40ce39fdb169c",
+    measurementId: "G-Y8Y2C59CR2"
+  };
